cf6a75593ea4dcfc9977e20e681e873ad797d8ab
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-03 13:45:31 -0700

    working on server and html

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
 app.use(express.static(__dirname + "/public"));
-const server = app.listen(5000);
+
+const server = app.listen(5000, function(){
+    console.log('listening on 3000');
+});
+
 const io = require('socket.io')(server);
