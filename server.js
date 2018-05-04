4e57774dcba207e9aac350b0f2312d259eb473c4
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-04 06:48:13 -0700

    emojis are able to sent and display, but need to debug

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 const express = require("express");
-var emojis = require('emojis-unicode');
-console.log(emojis[0]);
+// var emojis = require('emojis-unicode');
+// console.log(emojis[0]);
 const app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/static"));
 
-const server = app.listen(5000, function(){
+const server = app.listen(5000, function () {
     console.log('listening on 5000');
 });
 
 const io = require('socket.io')(server);
 
-var users = {};
+var chat = [];
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
-    socket.on('disconnect', function(){
+    io.emit('chat message', chat);
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
-    socket.on('login', function(users){
-        console.log('username: ' +users);
+    socket.on('login', function (users) {
+        console.log('username: ' + users);
         io.emit('login', users)
     });
-    socket.on('chat message', function(msg){
-        console.log('message: ' +msg);
-        io.emit('chat message', msg)
+    socket.on('chat message', function (data) {
+        console.log('message: ' + data.msg);
+        chat.push(data);
+        io.emit('chat message', chat);
     });
-  });
-
+});
